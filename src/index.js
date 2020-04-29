7690c07a2969be291265ce674d39a7aaf48fd361
Ewerton Nascimento <ewertonorg@gmail.com>
2020-04-28 23:18:54 -0300

    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -3,7 +3,41 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json('Hello World!');
+  return response.json('Hello World with Nodemon!');
 });
 
-app.listen(3333);
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'
+  ]);
+});
+
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3'
+  ]);
+});
+
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 2',
+    'Projeto 3'
+  ]);
+});
+
+app.listen(3333, ()=>{
+  console.log('Cool 🚀')
+});
