50a004334c1cc2522f84dd620ee193a0f61fab04
Ewerton Nascimento <ewertonorg@gmail.com>
2020-04-30 23:25:36 -0300

    Middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
 const projects = [];
+
+function logRequests(request, response, next) {
+  const {method, url} = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`
+  console.log(logLabel);
+
+  return next();
+
+}
+
+function validateProjectId(request, response, next) {
+  const {id} = request.params;
+  
+  if(!isUuid(id)){
+    return response.status(404).json({error:'Invalid project id'})
+  }
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
+
 
 app.get('/projects', (request, response) => {  
   const { title } = request.query;
@@ -69,6 +91,6 @@ app.delete('/projects/:id', (request, response) => {
   return response.status(204).send();
 });
 
-app.listen(3333, ()=>{
+app.listen(3333, () => {
   console.log('Cool 🚀')
 });
